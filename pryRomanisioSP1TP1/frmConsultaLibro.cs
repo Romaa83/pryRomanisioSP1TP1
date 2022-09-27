101da6289474a5ab886e6f629f4ce3278678400a
Romaa83 <agustin_roma@outlook.com>
2022-09-27 18:12:25 -0300

    cambio

--- pryRomanisioSP1TP1/frmConsultaLibro.cs
+++ pryRomanisioSP1TP1/frmConsultaLibro.cs
@@ -21,7 +21,7 @@ namespace pryRomanisioSP1TP1
 
         public frmConsultaLibro()
         {
-            InitializeComponent();
+            InitializeComponent();         
         }
 
         private void frmConsultaLibro_Load(object sender, EventArgs e)
@@ -30,9 +30,9 @@ namespace pryRomanisioSP1TP1
             int fm = 0;
             int fmDistribuidora = 0;
             int fmEditorial = 0;
-            StreamReader srDistribuidora = new StreamReader("./Distribuidora.txt");
+            StreamReader srDistribuidora = new StreamReader("./pryRomanisioSP1TP1/DISTRIBUIDORA");
             StreamReader srConsultaLibro = new StreamReader("./LIBRO.txt");
-            StreamReader srEditorial = new StreamReader("./Editorial.txt");
+            StreamReader srEditorial = new StreamReader("./EDITORIAL.txt");
 
             while (!srDistribuidora.EndOfStream)
             {
@@ -50,22 +50,22 @@ namespace pryRomanisioSP1TP1
             }
             while (!srConsultaLibro.EndOfStream)
             {
-                string[] arrayConsultaLibro = srConsultaLibro.ReadLine().Split(separador); 
+                string[] arrayConsultaLibro = srConsultaLibro.ReadLine().Split(separador);
                 matConsultaLibro[fm, 0] = arrayConsultaLibro[0]; //Le agrego a la matriz los datos del array
                 matConsultaLibro[fm, 1] = arrayConsultaLibro[1];
                 matConsultaLibro[fm, 2] = arrayConsultaLibro[2];
                 matConsultaLibro[fm, 3] = arrayConsultaLibro[3];
-                matConsultaLibro[fm, 4] = arrayConsultaLibro[4];           
+                matConsultaLibro[fm, 4] = arrayConsultaLibro[4];
 
-                switch (matConsultaLibro[fm,4]) //Segun el numero que tenga la matriz, va a ser el texto de la matriz
+                switch (matConsultaLibro[fm, 4]) //Segun el numero que tenga la matriz, va a ser el texto de la matriz
                 {
-                    case "\t1":matConsultaLibro[fm, 4] = matDistribuidora[0, 1]; break;
-                    case "\t2":matConsultaLibro[fm, 4] = matDistribuidora[1, 1]; break;
-                    case "\t3":matConsultaLibro[fm, 4] = matDistribuidora[2, 1];break;
-                    case "\t4":matConsultaLibro[fm, 4] = matDistribuidora[3, 1];break;
-                    case "\t5":matConsultaLibro[fm, 4] = matDistribuidora[4, 1];break;
-                    case "\t6":matConsultaLibro[fm, 4] = matDistribuidora[5, 1];break;
-                    case "\t7":matConsultaLibro[fm, 4] = matDistribuidora[6, 1];break;
+                    case "\t1": matConsultaLibro[fm, 4] = matDistribuidora[0, 1]; break;
+                    case "\t2": matConsultaLibro[fm, 4] = matDistribuidora[1, 1]; break;
+                    case "\t3": matConsultaLibro[fm, 4] = matDistribuidora[2, 1]; break;
+                    case "\t4": matConsultaLibro[fm, 4] = matDistribuidora[3, 1]; break;
+                    case "\t5": matConsultaLibro[fm, 4] = matDistribuidora[4, 1]; break;
+                    case "\t6": matConsultaLibro[fm, 4] = matDistribuidora[5, 1]; break;
+                    case "\t7": matConsultaLibro[fm, 4] = matDistribuidora[6, 1]; break;
                 }
                 switch (matConsultaLibro[fm, 2])
                 {
@@ -80,11 +80,11 @@ namespace pryRomanisioSP1TP1
                 fm++;
             }
             srConsultaLibro.Close();
-                   dgvConsultaLibros.Rows.Add(matConsultaLibro[NumPag, 0],
-                   matConsultaLibro[NumPag, 1],
-                   matConsultaLibro[NumPag, 2],
-                   matConsultaLibro[NumPag, 3],
-                   matConsultaLibro[NumPag, 4]);
+            dgvConsultaLibros.Rows.Add(matConsultaLibro[NumPag, 0],
+            matConsultaLibro[NumPag, 1],
+            matConsultaLibro[NumPag, 2],
+            matConsultaLibro[NumPag, 3],
+            matConsultaLibro[NumPag, 4]);
         }
 
         private void btnSiguiente_Click(object sender, EventArgs e)
